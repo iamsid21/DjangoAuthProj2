1846b03712cfe00789d117cd3344eb9f4161cdb6
Siddharth <iamsiddharthpathak@gmail.com>
2021-09-19 01:19:57 +0530

    added sk

--- usersapp/settings.py
+++ usersapp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = 'django-insecure-@1ono79)&abjc$db427a)&o=*epxbpslv65d3ztr$av!+8zy+('
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
